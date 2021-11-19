fbdd36dc0df5620f9ab61e5ea9f3de77054d52f6
zhoumengbo <zhoumengbo1106@163.com>
2021-11-19 20:27:02 +0800

    一些连接参数

---
+++ testSpark/src/main/scala/com/dsj/spark/streaming/Constants.scala
@@ -0,0 +1,9 @@
+package com.dsj.spark.streaming
+/**
+ * 定义数据连接参数
+ */
+object Constants {
+  var url:String = "jdbc:mysql://172.19.240.19:3306/test"
+  var userName:String = "hive"
+  var passWord:String = "hive"
+}
